e94cb1cff1e3e2b8f5a0358f7f16cc05004728e2
Tolxxy <precious.oguntuase@binghamuni.edu.ng>
2024-11-21 21:33:48 +0100

    spring

---
+++ src/main/java/com/example/example/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {    
+    User findByFullName(String fullName);
+}
+
